f8052b7ee27ef8112b2156727b632ae576835dc6
Raiane <raiane.oliveira404@gmail.com>
2023-04-07 18:38:14 -0300

    feat: API call data weather

--- src/App.js
+++ src/App.js
@@ -4,10 +4,36 @@ import AirQuality from "./components/AirQuality";
 import SunTime from "./components/SunTime";
 import WeekWeather from "./components/WeekWeather";
 
+import { API_URL } from "./api";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
+  const [weather, setWeather] = useState({});
+
+  async function getWeather() {
+    const response = await axios
+      .get(`${API_URL}&q=Campina%20Grande`)
+      .then((result) => result.data);
+    setWeather(response);
+  }
+
+  useEffect(() => {
+    getWeather();
+  }, []);
+
+  console.log(weather);
+
   return (
     <main className="App">
-      <TemperatureNow />
+      <TemperatureNow
+        cityName={weather && weather.name}
+        countryName={weather && weather.sys}
+        windSpeed={weather && weather.wind}
+        temp={weather && weather.main}
+        weather={weather && weather.weather[0]}
+        clouds={weather && weather.clouds}
+      />
       <AirQuality />
       <SunTime />
       <WeekWeather />
--- src/components/TemperatureNow.js
+++ src/components/TemperatureNow.js
@@ -1,62 +1,70 @@
-import React from "react";
+import React, { useContext } from "react";
 import location from "../assets/location.svg";
 import wind from "../assets/wind.svg";
 import humidity from "../assets/humidity.svg";
 import cloudsPurple from "../assets/cloudsPurple.svg";
 
-export default function TemperatureNow() {
-    return (
-        <article className="box-weather temperature-now">
-            <section className="location-temperature-now">
-                <img src={location} alt="Location figure" />
-                <p>Rio do Sul, SC</p>
-            </section>
+export default function TemperatureNow(props) {
+  const { cityName, countryName, temp, weather, windSpeed, clouds } = props;
+  console.log(props);
+  return (
+    <article className="box-weather temperature-now">
+      <section className="location-temperature-now">
+        <img src={location} alt="Location figure" />
+        <p>
+          {cityName + "," ?? "Não localizado"} {countryName.country ?? ""}
+        </p>
+      </section>
 
-            <section className="temperature-temperature-now">
-                <h1 className="title-temperature">
-                    18
-                    <span className="degrees-title-temperature"> °C</span>
-                </h1>
-                <span className="first-degrees-box-weather">22°</span>
-                <span className="second-degrees-box-weather">16°</span>
-            </section>
+      <section className="temperature-temperature-now">
+        <h1 className="title-temperature">
+          {temp && Math.round(temp.temp)}
+          <span className="degrees-title-temperature"> °C</span>
+        </h1>
+        <span className="first-degrees-box-weather">
+          {weather &&
+            weather.description.replace(/(?:^|\s)\S/g, (letter) =>
+              letter.toUpperCase()
+            )}
+        </span>
+      </section>
 
-            <section className="statistics-temperature-now flex">
-                <StatisticTemperatureNow
-                    img={wind}
-                    imgAlt="Ilustração vento"
-                    title="Vento"
-                    number={17}
-                    symbol="km/h"
-                />
-                <StatisticTemperatureNow
-                    img={humidity}
-                    imgAlt="Ilustração umidade"
-                    title="Umidade"
-                    number={31}
-                    symbol="%"
-                />
-                <StatisticTemperatureNow
-                    img={cloudsPurple}
-                    imgAlt="Ilustração Chuva"
-                    title="Chuva"
-                    number={10}
-                    symbol="%"
-                />
-            </section>
-        </article>
-    );
+      <section className="statistics-temperature-now flex">
+        <StatisticTemperatureNow
+          img={wind}
+          imgAlt="Ilustração vento"
+          title="Vento"
+          number={windSpeed.speed}
+          symbol="m/s"
+        />
+        <StatisticTemperatureNow
+          img={humidity}
+          imgAlt="Ilustração umidade"
+          title="Umidade"
+          number={temp.humidity}
+          symbol="%"
+        />
+        <StatisticTemperatureNow
+          img={cloudsPurple}
+          imgAlt="Ilustração Chuva"
+          title="Nebulosidade"
+          number={clouds.all}
+          symbol="%"
+        />
+      </section>
+    </article>
+  );
 }
 
 function StatisticTemperatureNow({ img, imgAlt, title, number, symbol }) {
-    return (
-        <div className="box-statistics">
-            <img src={img} alt={imgAlt} />
-            <p className="title-box-statistics">{title}</p>
-            <p className="number-box-statistics">
-                {number}
-                <span className="symbol-box-statistics">{symbol}</span>
-            </p>
-        </div>
-    );
+  return (
+    <div className="box-statistics">
+      <img src={img} alt={imgAlt} />
+      <p className="title-box-statistics">{title}</p>
+      <p className="number-box-statistics">
+        {number}
+        <span className="symbol-box-statistics">{symbol}</span>
+      </p>
+    </div>
+  );
 }
